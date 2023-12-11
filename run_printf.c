cd20ba27dedcdf35f7f2188ed41731833d768c26
Sindi <sindiswamsubo71@gmail.com>
2023-12-11 02:08:07 -0800

    print updated

--- run_printf.c
+++ run_printf.c
@@ -10,5 +10,18 @@
 
 void run_printf(int fd, char *str)
 {
-	write(fd, str, strlen(str));
+	size_t len = _strlen(str);
+	char *new_str = malloc(len + 2);
+
+	if (new_str == NULL)
+	{
+		return;
+	}
+
+	_strncpy(new_str, str);
+	new_str[len] = '\n';
+	new_str[len + 1] = '\0';
+
+	write(fd, new_str, len + 1);
+	free(new_str);
 }
